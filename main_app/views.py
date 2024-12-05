5a108858c20bfde0ace78c158caeb720e0aa80e4
JakeMackBrown <JakeMackBrown@gmail.com>
2024-12-05 13:28:52 -0600

    module 10

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
 
 # Define the home view function
@@ -12,7 +13,20 @@ def cat_index(request):
     cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
                   
-
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url='/cats/'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
